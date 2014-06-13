5dc076581ab0bfad14148b1e0f8069c269eab577
Alex Baker <al3xb4k3r@gmail.com>
2014-06-12 23:15:20 -0600

    moved code around, switched to google admin mechinism, deleted unnecesary code, added comments

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import os
 
 import webapp2
 import jinja2
+from google.appengine.api import users
 
 import tools
 import db
@@ -13,6 +14,7 @@ jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir),
 
 
 class Handler(webapp2.RequestHandler):
+    """General handler helper functions"""
     def write(self, *a, **kw):
         self.response.out.write(*a, **kw)
 
@@ -23,22 +25,17 @@ class Handler(webapp2.RequestHandler):
     def render(self, template, **kw):
         self.write(self.render_str(template, **kw))
 
-    def render_json(self, d):
-        json_txt = json.dumps(d)
-        self.response.headers['Content-Type'] = 'application/json; charset=UTF-8'
-        self.write(json_txt)
-
     def make_cookie(self, name, val):
         cookie_val = tools.make_secure_val(str(val))
         return self.response.headers.add_header(
-                        'Set-Cookie',
-                        '%s=%s; Path=/' % (name, cookie_val)
-                        )
+                                    'Set-Cookie',
+                                    '%s=%s; Path=/' % (name, cookie_val)
+                                    )
 
     def read_cookie(self):
-        user_id = self.request.cookies.get('user_id')
-        if user_id:
-            return tools.check_secure_val(user_id)
+        user_key = self.request.cookies.get('user_key')
+        if user_key:
+            return tools.check_secure_val(user_key)
 
 
 class Front(Handler):
@@ -46,33 +43,64 @@ class Front(Handler):
         self.render('front.html')
 
 
-class NewPost(Handler):
-    def render_newpost(self, subject="", content="", error=""):
-        self.render('newpost.html', subject=subject, content=content, error=error)
-
+class StudentPortal(Handler):
     def get(self):
-        self.render_newpost()
+        user_key = self.read_cookie()
+        if not user_key:
+            user_key = self.request.get('key')
+        student = db.get_student(user_key)
+        if user_key or users.is_current_user_admin():
+            self.render('student.html', name=student.user)
+        else:
+            self.redirect('/')
+
+
+class Login(Handler):
+    def get(self):
+        user_key = self.read_cookie()
+        if user_key:
+            self.redirect('/student')
+        else:
+            self.render('login.html')
 
     def post(self):
-        subject = self.request.get('subject')
-        content = self.request.get('content')
-        if subject and content:
-            notes = db.write_notes(subject = subject,
-                                  subject_uri = subject_uri,
-                                  content = content)
-            self.redirect('/%d' % blog.key.id())
-        else:
-            error = "Please enter a subject AND content!"
-            self.render_newpost(subject, content, error)
+        user_name = self.request.get('username')
+        user_pw = self.request.get('password')
+        user_entry = db.Students.by_user(user_name).get()
+        if user_entry:
+            user_key = user_entry.key.urlsafe()
+            hashed = user_entry.password
+            if tools.bcrypt.hashpw(user_pw, hashed) == hashed:
+                self.make_cookie('user_key', user_key)
+                self.redirect('/student')
+        self.render('login.html', login_error='Invalid Login')
+
+
+class Logout(Handler):
+    def get(self):
+        self.response.delete_cookie('user_key')
+        self.redirect('/')
+
+
+class AdminPortal(Handler):
+    """===admin only access===
+
+    This is the admin portal, can signup new students and add
+    lesson notes here.
+    """
+    def get(self):
+        students = db.get_all_students()
+        self.render('admin.html', students=students)
 
 
 class NewStudent(Handler):
+    """===admin only access===
+
+    get - checks for admin and renders form
+    post - validates and creates a new student entity
+    """
     def get(self):
-        user_id = self.read_cookie()
-        if tools.is_admin(user_id):
-            self.render('newstudent.html')
-        else:
-            self.redirect('/')
+        self.render('newstudent.html')
 
     def post(self):
         have_error = False
@@ -101,60 +129,46 @@ class NewStudent(Handler):
             self.render('newstudent.html', **params)
         else:
             hashed_pw = tools.password_protect(user_pw)
-            user = db.write_user(user = user_name,
-                                  password = hashed_pw,
-                                  email = user_email
-                                )
-            self.make_cookie('user_id', user.id())
-            self.redirect('/welcome')
+            user = db.write_user(
+                            user = user_name,
+                            password = hashed_pw,
+                            email = user_email
+                            )
+            self.redirect('/admin')
 
 
-class Login(Handler):
+class NewNotes(Handler):
+    """===admin only access===
+
+    get -- checks for admin and renders lesson notes form
+    post -- validates and creates a new lesson notes entry
+    """
+    def render_newpost(self, subject="", content="", error=""):
+        self.render('newnotes.html', subject=subject, content=content, error=error)
+
     def get(self):
-        user_id = self.read_cookie()
-        if user_id:
-            if tools.is_admin(user_id):
-                self.render('admin.html')
-            else:
-                self.render('student-portal.html')
-        else:
-            self.render('login.html')
+        self.render_newpost()
 
     def post(self):
-        user_name = self.request.get('username')
-        user_pw = self.request.get('password')
-        user_entry = db.Students.by_user(user_name).get()
-        if user_entry:
-            user_id = user_entry.key.id()
-            hashed = user_entry.password
-            if tools.bcrypt.hashpw(user_pw, hashed) == hashed:
-                self.make_cookie('user_id', user_id)
-                self.redirect('/lesson-portal')
-        self.render('login.html', login_error='Invalid Login')
-
-
-class Logout(Handler):
-    def get(self):
-        self.response.delete_cookie('user_id')
-        self.redirect('/')
-
-
-class LessonPortal(Handler):
-    def get(self):
-        user_id = self.read_cookie()
-        if tools.is_admin(user_id):
-            students = db.get_students()
-            self.render('admin.html', students=students)
-        elif user_id:
-            self.render('student.html')
+        subject = self.request.get('subject')
+        content = self.request.get('content')
+        if subject and content:
+            notes = db.write_notes(
+                                subject = subject,
+                                content = content
+                                )
+            self.redirect('/%d' % blog.key.id())
         else:
-            self.redirect('/')
+            error = "Please enter a subject AND content!"
+            self.render_newpost(subject, content, error)
 
 
-app = webapp2.WSGIApplication([('/', Front),
-                               ('/new-notes', NewNotes),
-                               ('/newstudent', NewStudent),
-                               ('/login', Login),
-                               ('/logout', Logout),
-                               ('/lesson-portal', LessonPortal)
-                            ], debug=True)
+app = webapp2.WSGIApplication([
+                        ('/', Front),
+                        ('/newnotes', NewNotes),
+                        ('/newstudent', NewStudent),
+                        ('/login', Login),
+                        ('/logout', Logout),
+                        ('/student', StudentPortal),
+                        ('/admin', AdminPortal)
+                        ], debug=True)
