6687efeeb7fb32a0f2d29a13425efb4f6e1d7500
Alex Baker <al3xb4k3r@gmail.com>
2014-06-15 01:15:57 -0600

    minor name and comment changes, added lesson notes datastore entry, added full name to student entry

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 import os
+import logging
 
 import webapp2
 import jinja2
 from google.appengine.api import users
 
 import tools
-import db
+import ds
 
 
 template_dir = os.path.join(os.path.dirname(__file__), 'templates')
@@ -14,7 +15,7 @@ jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir),
 
 
 class Handler(webapp2.RequestHandler):
-    """General handler helper functions"""
+    """General helper functions"""
     def write(self, *a, **kw):
         self.response.out.write(*a, **kw)
 
@@ -26,10 +27,10 @@ class Handler(webapp2.RequestHandler):
         self.write(self.render_str(template, **kw))
 
     def make_cookie(self, name, val):
-        cookie_val = tools.make_secure_val(str(val))
+        cookie_val = tools.make_secure_val(val)
         return self.response.headers.add_header(
                                     'Set-Cookie',
-                                    '%s=%s; Path=/' % (name, cookie_val)
+                                    '%s=%s; Path=/; HttpOnly' % (name, cookie_val)
                                     )
 
     def read_cookie(self):
@@ -48,9 +49,11 @@ class StudentPortal(Handler):
         user_key = self.read_cookie()
         if not user_key:
             user_key = self.request.get('key')
-        student = db.get_student(user_key)
+        notes = ds.get_notes(user_key)
+        student = ds.get_student(user_key)
+        first_name = student.name.split()[0]
         if user_key or users.is_current_user_admin():
-            self.render('student.html', name=student.user)
+            self.render('student.html', name=first_name, notes=notes)
         else:
             self.redirect('/')
 
@@ -66,7 +69,7 @@ class Login(Handler):
     def post(self):
         user_name = self.request.get('username')
         user_pw = self.request.get('password')
-        user_entry = db.Students.by_user(user_name).get()
+        user_entry = ds.Students.by_user(user_name).get()
         if user_entry:
             user_key = user_entry.key.urlsafe()
             hashed = user_entry.password
@@ -85,35 +88,38 @@ class Logout(Handler):
 class AdminPortal(Handler):
     """===admin only access===
 
-    This is the admin portal, can signup new students and add
+    This is the admin portal, can sign-up new students and add
     lesson notes here.
     """
     def get(self):
-        students = db.get_all_students()
+        students = ds.get_all_students()
         self.render('admin.html', students=students)
 
 
-class NewStudent(Handler):
+class AddStudent(Handler):
     """===admin only access===
 
-    get - checks for admin and renders form
-    post - validates and creates a new student entity
+    Validates and creates a new student entity
     """
     def get(self):
-        self.render('newstudent.html')
+        self.render('addstudent.html')
 
     def post(self):
         have_error = False
+        name = self.request.get('name')
         user_name = self.request.get('username')
         user_pw = self.request.get('password')
         verify = self.request.get('verify')
         user_email = self.request.get('email')
         params = dict(username = user_name,
                       email = user_email)
-        if not tools.valid_username(user_name):
-            params['name_error'] = "That's not a valid name!"
+        if not name:
+            params['name_error']= "Please enter a name!"
             have_error = True
-        if db.Students.by_user(user_name).get():
+        if not tools.valid_username(user_name):
+            params['username_error'] = "That's not a valid name!"
+            have_error = True
+        if ds.Students.by_user(user_name).get():
             params['name_error'] = "That user already exists!"
             have_error = True
         if not tools.valid_password(user_pw):
@@ -126,47 +132,55 @@ class NewStudent(Handler):
             params['email_error'] = "That's not a valid email!"
             have_error = True
         if have_error:
-            self.render('newstudent.html', **params)
+            self.render('addstudent.html', **params)
         else:
             hashed_pw = tools.password_protect(user_pw)
-            user = db.write_user(
+            user = ds.write_user(
                             user = user_name,
                             password = hashed_pw,
-                            email = user_email
+                            email = user_email,
+                            name = name
                             )
             self.redirect('/admin')
 
 
-class NewNotes(Handler):
+class AddNotes(Handler):
     """===admin only access===
 
-    get -- checks for admin and renders lesson notes form
-    post -- validates and creates a new lesson notes entry
+    Validates and creates a new lesson notes entry
     """
-    def render_newpost(self, subject="", content="", error=""):
-        self.render('newnotes.html', subject=subject, content=content, error=error)
-
     def get(self):
-        self.render_newpost()
+        user_key = self.request.get('key')
+        student = ds.get_student(user_key)
+        self.render('addnotes.html', student=student)
 
     def post(self):
-        subject = self.request.get('subject')
-        content = self.request.get('content')
-        if subject and content:
-            notes = db.write_notes(
-                                subject = subject,
-                                content = content
+        student = self.request.get('key')
+        warmup = self.request.get('warmup')
+        assign = self.request.get('assign')
+        tips = self.request.get('tips')
+        if student and warmup and assign and tips:
+            notes = ds.write_notes(
+                                student = student,
+                                warmup = warmup,
+                                tips = tips,
+                                assign = assign
                                 )
-            self.redirect('/%d' % blog.key.id())
+            self.redirect('/admin')
         else:
-            error = "Please enter a subject AND content!"
-            self.render_newpost(subject, content, error)
+            error = "Please fill out all fields!"
+            self.render('addnotes.html',
+                        student = student,
+                        warmup = warmup,
+                        tips = tips,
+                        assign = assign,
+                        error = error)
 
 
 app = webapp2.WSGIApplication([
                         ('/', Front),
-                        ('/newnotes', NewNotes),
-                        ('/newstudent', NewStudent),
+                        ('/addnotes', AddNotes),
+                        ('/addstudent', AddStudent),
                         ('/login', Login),
                         ('/logout', Logout),
                         ('/student', StudentPortal),
