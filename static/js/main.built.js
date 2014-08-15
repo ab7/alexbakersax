1b3a838a4ed5bb18b9b1898fbb2cfda99ee941e2
Alex Baker <al3xb4k3r@gmail.com>
2014-08-15 02:03:12 -0600

    fullpage.js now disabled less than 500px, using url hash for live window resizing

--- static/js/main.built.js
+++ static/js/main.built.js
@@ -28179,18 +28179,39 @@ var tooltip = $.widget( "ui.tooltip", {
 
 })(jQuery);
 
-/*global $, alert, console*/
+/*global $, alert, console, matchMedia*/
+
+// media query change
+function widthChange(mq) {
+  'use strict';
+  var hash;
+  hash = window.location.hash;
+  if (mq.matches) {
+    // window width is at least 500px
+    window.location.hash = '#full';
+    $('#fullpage').fullpage({
+      css3: true,
+      loopHorizontal: false,
+      resize: false,
+      verticalCentered: false
+    });
+  } else {
+    if (hash !== '#mobile') {
+      window.location.hash = '#mobile';
+      location.reload();
+    }
+  }
+}
 
 // initialize
 $(document).ready(function () {
   'use strict';
-  $('#fullpage').fullpage({
-    anchors: ['#front', '#new-student-info', '#materials'],
-    css3: true,
-    loopHorizontal: false,
-    navigation: true,
-    resize: false,
-    verticalCentered: false
-  });
+
+  // media query event handler
+  if (matchMedia) {
+    var mq = window.matchMedia("(min-width: 500px)");
+    mq.addListener(widthChange);
+    widthChange(mq);
+  }
   $('.circleWrap').lettering();
 });
